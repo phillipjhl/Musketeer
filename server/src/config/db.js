b34968bf82e061d14232c4941bea4dcd52536563
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-23 23:09:57 -0500

    setup database connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,9 +3,9 @@ import mysql from 'mysql';
 let pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
-    user: 'exampleUser',
-    password: 'password',
-    database: 'InClassExample'
+    user: 'blogapp',
+    password: 'blogauthentication',
+    database: 'blog'
 });
 
 async function executeQuery(sql, args = []) {
