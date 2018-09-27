657dab617775aac6cd7e055473ac4b1cf97f21c2
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-27 01:11:14 -0500

    navbar for whole app

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 function NavBar(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
             <Link to='/' className="navbar-brand" >Blog</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
