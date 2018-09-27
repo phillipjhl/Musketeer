41e16538e15ebc90df74e986bbd2355130008cd6
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-27 11:23:10 -0500

    added token authorization on API

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import authRouter from './auth';
+import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
 import blogsRouter from './blogs';
 
 let router = Router();
+
+router.use('/auth', authRouter);
+
+router.route('*')
+    .post(tokenMiddleware, isLoggedIn)
+    .put(tokenMiddleware, isLoggedIn)
+    .delete(tokenMiddleware, isLoggedIn);
 
 router.use('/blogs', blogsRouter);
 
