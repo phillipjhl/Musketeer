18f7f307b7d6cb272c1db0437ef0348fc9cc9587
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-27 01:10:35 -0500

    main component to handle most child component rendering for home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Jumbotron from './Jumbotron';
+import Sidebar from './Sidebar';
+import BlogLists from './BlogList';
 
 class Home extends Component {
 
     constructor(props) {
         super(props);
     }
-    
+
     render() {
-        return <h1>Hello World!</h1>;
+        return (
+            <Fragment>
+                <Jumbotron />
+                <div className="row">
+                <BlogLists />
+                <Sidebar />
+                </div>
+            </Fragment>
+        );
     }
+
 }
 
 export default Home;
