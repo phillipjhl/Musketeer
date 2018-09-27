ee8852bb8c37ca3c25ca4cd42ca83940f7c72134
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-27 01:38:33 -0500

    set default mode to development

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CLIENT_DEST = path.join(__dirname, './client/dist');
 module.exports = {
     entry: ['@babel/polyfill', './client/src/index.js'],
     output: { path: CLIENT_DEST, filename: 'bundle.js' },
+    mode: 'development',
     module: {
         rules: [
             {
