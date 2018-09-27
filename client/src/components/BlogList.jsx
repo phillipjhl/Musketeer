3d324b9ecd0e2311cc6b0bd17ff5e5ce46b40e97
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-27 01:58:28 -0500

    added fetch for list of blogs

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -7,15 +7,37 @@ class BlogList extends Component {
     constructor(props) {
         super(props);
         this.state={
-            //list of blog components
+            //list of blog data
             blogList: []
         }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3000/api/blogs")
+            .then(res => res.json())
+            .then(blogs => {
+                console.log(blogs);
+                let updatedblogs = [];
+                blogs.forEach(blog => {
+                    let data = {
+                        id: blog.id,
+                        title: blog.title,
+                        content: blog.content,
+                        date: blog._created
+                    }
+                    updatedblogs.push(data);
+                });
+                this.setState({
+                    blogList: updatedblogs
+                });
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
         return (
             <Fragment>
-                <aside class="col-md-8 blog-main">
+                <aside className="col-md-8 blog-main">
                     <BlogShort />
                 </aside>
             </Fragment>
