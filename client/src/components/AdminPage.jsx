6900761ae39f90398a2c9f63691112a348c27dac
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-28 00:08:23 -0500

    added unfinsihed admin page

---
+++ client/src/components/AdminPage.jsx
@@ -0,0 +1,21 @@
+//Admin page to edit or delete blogs
+
+import React, { Component } from 'react';
+// import { all, one, insert, update, destroy } from '../services/blogs';
+
+class AdminPage extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            data: {}
+        };
+    }
+
+    render() {
+        return (
+            <h1>Hello there.</h1>
+        );
+    }
+}
+
+export default AdminPage;
