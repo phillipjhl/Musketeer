6ab6e0ba237591caced2186a57587429914724c0
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-28 00:05:14 -0500

    conditionally renders on isloggedin

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import Jumbotron from './Jumbotron';
 import Sidebar from './Sidebar';
 import BlogLists from './BlogList';
+import { isLoggedIn } from '../services/user';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
 
@@ -10,15 +12,28 @@ class Home extends Component {
     }
 
     render() {
-        return (
-            <Fragment>
-                <Jumbotron />
-                <div className="row">
-                <BlogLists />
-                <Sidebar />
-                </div>
-            </Fragment>
-        );
+        if (isLoggedIn()) {
+            return (
+                <Fragment>
+                    <Jumbotron />
+                    <Link to='/input' className="btn btn-info mb-1">Add New Blog Post</Link>
+                    <div className="row">
+                        <BlogLists />
+                        <Sidebar />
+                    </div>
+                </Fragment>
+            );
+        } else {
+            return (
+                <Fragment>
+                    <Jumbotron />
+                    <div className="row">
+                        <BlogLists />
+                        <Sidebar />
+                    </div>
+                </Fragment>
+            );
+        }
     }
 
 }
