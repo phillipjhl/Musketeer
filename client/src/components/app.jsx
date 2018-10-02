5f95506e6f79720fcade7486ef2a92180812a921
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-10-02 14:44:31 -0500

    added route for donation page

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 import NavBar from './NavBar';
 import Home from './Home';
 import AdminInput from './AdminInput';
@@ -8,6 +9,7 @@ import PrivateRoute from './auth/privateRoute';
 import Login from './auth/login';
 import Logout from './auth/logout';
 import AdminEdit from './AdminEdit';
+import Donate from './donate';
 
 class Navigation extends Component {
 
@@ -21,6 +23,7 @@ class Navigation extends Component {
                         <Route path="/login" component={Login} />
                         <Route path="/logout" component={Logout} />
                         <Route path ="/blogs/:id" component={BlogFull} />
+                        <Route path="/donate" component={Donate} />
                         <PrivateRoute path="/admin" component={AdminInput} />
                         <PrivateRoute exact path="/blog/:id/edit" component={AdminEdit} />
                     </Switch>
