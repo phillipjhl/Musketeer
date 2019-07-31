192ebdb1cc3eb22aaa9aec1deab9586497e0a151
Phillip Langland <Phillip.Langland@chiprewards.com>
2019-07-31 12:11:35 -0500

    Merge develop
    Updating package.json and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                 },
             },
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|svg|gif)$/,
                 use: {
                     loader: 'file-loader',
                     options: {
