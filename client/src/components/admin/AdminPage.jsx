cd42050058b622f1bc3e817e5e121357afcf44d8
Phillip Langland <Phillip.Langland@chiprewards.com>
2019-08-04 03:39:08 -0500

    rough UserContext api

--- client/src/components/admin/AdminPage.jsx
+++ client/src/components/admin/AdminPage.jsx
@@ -1,29 +1,30 @@
 //Admin page to edit or delete blogs
-
 import React, { Fragment, Component } from "react";
+import UserContext from "../../services/context";
 // import { all, one, insert, update, destroy } from '../services/blogs';
 
 import { AdminEdit } from "./AdminEdit";
 import AdminInput from "./AdminInput";
-
 class AdminPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: props.user
+      user: this.context
     };
   }
 
+  static contextType = UserContext;
+
   render() {
+    console.log(this.context);
     return (
       <Fragment>
         <div className="container-fluid">
-          <h1>Hello there.</h1>
+          <h1>{`Hello there, ${this.context.first_name}`}</h1>
           <AdminInput />
         </div>
       </Fragment>
     );
   }
 }
-
 export default AdminPage;
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { checkLogin } from "../services/user";
+import { checkLogin, getUser } from "../services/user";
 
 import NavBar from "./NavBar";
 import Home from "./Home";
@@ -13,46 +13,53 @@ import Login from "./auth/login";
 import Logout from "./auth/logout";
 import Donate from "./donate";
 import Contact from "./contact";
-
+import UserContext from '../services/context';
+// export const UserContext = React.createContext("guest");
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: undefined
+      user: 'guest'
     };
   }
+  // static contextType = UserContext;
 
   componentDidMount() {
     checkLogin()
       .then(loggedIn => {
-        this.setState({ user });
+        getUser()
+          .then(user => {
+            console.log(user);
+            this.setState({ user });
+          })
+          .catch(err => {
+            throw err;
+          });
       })
       .catch(err => console.error(err));
   }
 
   render() {
     return (
-      <Router>
-        <Fragment>
-          <NavBar user={this.state.user} />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/blogs/:id" component={BlogFull} />
-            <Route path="/donate" component={Donate} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/blogs" component={Home} />
-            <PrivateRoute
-              path="/admin"
-              component={AdminPage}
-              user={this.state.user}
-            />
-            <PrivateRoute exact path="/blog/:id/edit" component={AdminEdit} />
-          </Switch>
-        </Fragment>
-      </Router>
+      <UserContext.Provider value={this.state.user}>
+        <Router>
+          <Fragment>
+            <NavBar user={this.state.user} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/blogs/:id" component={BlogFull} />
+              <Route path="/donate" component={Donate} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blogs" component={Home} />
+              <PrivateRoute path="/admin" component={AdminPage} />
+              <PrivateRoute exact path="/blog/:id/edit" component={AdminEdit} />
+            </Switch>
+          </Fragment>
+        </Router>
+      </UserContext.Provider>
     );
   }
 }
